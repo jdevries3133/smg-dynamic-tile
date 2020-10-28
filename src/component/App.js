7b75281f8bbbe593f118f7d18f04d6d3a8a6e485
JohnDeVries <jdevries3133@gmail.com>
2020-10-27 21:55:49 -0400

    removed boilerplate

---
+++ src/component/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+const App = (props) => {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/renderer/index.js
@@ -0,0 +1,14 @@
+let midiParser = require('midi-parser-js');
+let fs = require('fs')
+
+const SMG_URL = 'https://musiclab.chromeexperiments.com/Song-Maker/song/5703199099977728'
+
+fs.readFile('sample_data/song.mid', 'base64', function (err, data) {
+  let midiArray = midiParser.parse(data);
+  midiArray.track.forEach((i) => {
+    i.event.forEach((i) => {
+      console.log(i)
+    })
+  })
+});
+
