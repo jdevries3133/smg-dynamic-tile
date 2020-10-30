27dcd931f7866d0f8ce995409653607cd60803c0
JohnDeVries <jdevries3133@gmail.com>
2020-10-30 13:58:47 -0400

    wrapped midi parsing and fetching in useEffect

--- src/DynamicTile/DynamicTile.js
+++ src/DynamicTile/DynamicTile.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Song } from "./MidiParser/Parser";
 
 export const DynamicTile = (props) => {
   // put song inside useEffect to make it not block the UI thread, and assign
   // it a ref or something so that it doesn't change on every render.
-  const song = new Song(props.url);
-  song.parse();
+  const [song, setSong] = useState(null);
+  useEffect(() => {
+    setSong(new Song(props.url));
+  }, [props.url]);
+  if (song) song.parse();
   return (
     <div>
+      {song ? <h1>yes</h1> : <h1>no</h1>}
       <h1>Dynamic Tile</h1>
     </div>
   );
--- src/DynamicTile/MidiParser/Parser.js
+++ src/DynamicTile/MidiParser/Parser.js
@@ -7,11 +7,18 @@ export class Song {
   }
   parse() {
     getSongData(this.url).then((data) => {
-      this.songData = data;
+      this._songData = data;
       this.parseFetchedData();
+      this.assignJsonAsAttrs();
     });
   }
   parseFetchedData() {
-    this.midiParsed = MidiParser.parse(this.songData.midiBytes);
+    this.midiParsed = MidiParser.parse(this._songData.midiBytes);
+  }
+  assignJsonAsAttrs() {
+    // lifts the attributes in the json file to class attributes
+    Object.keys(this._songData.json).forEach((i) => {
+      this[i] = this._songData.json[i];
+    });
   }
 }
